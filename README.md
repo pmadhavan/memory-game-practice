7e944b332fb76c69bd6e66735c1d6df0bd0b7863
pmadhavan <madhavan.padmaja@gmail.com>
2024-05-23 13:04:25 -0700

    Iteration 2, finished the game basic logic, with 1 bug.

--- README.md
+++ README.md
@@ -11,5 +11,15 @@ Things to look up
 To improve
 
 1. Couldn't finish within 45mins, first commit is the state i was after 45mins.
+    - Do not get stuck by shuffle algo or randomness, move quickly assuming you can use a librray or get back when time permits. Lodash has shuffle
+2. Can i pass state variables for example clickCount and setClickCount down the child component?
+3. Efficient and optimized way to update state of array of object, updating the array, updating the object prop single or multiple/
+4. I had extreme difficluty in finding the game state, started with child component and refactored multiple time to achive the flip of the card logic. A big red flag
+5. I was able to finish the problem, atleast the first usecase in 2 hours (red flag), but there is a bug. 
+    - I did not clear the matches after game finish
+    - The cards are not disapperaing after the game.
+    - Flipping back bothe the cards after incorrect match is also wrong, i updated only 1 item from macth,
+
+6. Cards are not removign after successful match. Yet to try again.
 
 
--- src/GameBoard.tsx
+++ src/GameBoard.tsx
@@ -1,22 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+interface itemProps {
+    id: number, value: number, isOpen:boolean, isRemoved: boolean
+}
 interface CardProps {
-value: number;
+item: itemProps
+onCardClickHandler: (item:itemProps)=> void;
 }
-const Card = ({value}:CardProps) => {
-    const [canShow, setCanShow] = useState(false);
-    return <div className="child" onClick={()=> setCanShow(true)}>{canShow && value}</div>
+const Card = ({item, onCardClickHandler}:CardProps) => {
+console.log("Rerenderig", item.value===2 && item);
+  if(item.isRemoved){
+    return <div>R</div>;
 }
+
+ return <div className={item.isRemoved ? 'blank': 'child'} onClick={()=> onCardClickHandler(item)}>{item.isOpen && item.value}</div>
+}
+
 
 const GameBoard = () => {
     const deck1 = [...Array(18).keys()].map((item) => item =  item+1);
-const GRID = [...Array(18).keys()].map((item) => item =  item+1).concat(deck1);
+    const GRID = [...Array(18).keys()].map((item) => item =  item+1).concat(deck1);
+    const initialGame = GRID.map((item, id)=> ({id:id, value:item, isOpen:false, isRemoved: false}));
+    const [matches, setMatches] = useState<itemProps[]>([]);
+    const [gameStatus, updateGameStatus] = useState<itemProps[]>(initialGame);
+    
+    const onCardClickHandler =(item:itemProps) => {
+        if(matches.length ===2) {
+            return;
+        }
+        const newMatches= [...matches, item]
+        setMatches(newMatches);
+        const newStatus = [...gameStatus].map((nitem) => {
+          if(nitem.id===item.id){
+            return {...nitem, isOpen:true}
+          }
+          return nitem;
+        });
+        updateGameStatus(newStatus);
+    }
+    const isMatch = ()=> {
+        if (matches[0] === matches[1]){
+            return true
+        }
+        return false;
+    }
+    const getUpdatedStatus = () => {
+        let newStatus;
+        if (isMatch()){
+             newStatus = [...gameStatus].map((item) => item.value === matches[0].value ? {...item, isRemoved:true}: item);
+        } else{
+            newStatus = [...gameStatus].map((item) => {
+              let nItem = {...item};
+              matches.forEach((match) => {
+                if(item.id=== match.id) {
+                    nItem= {...item, isOpen:false}
+                }
+              }
+              )  
+              return nItem;
 
+            });
+        }
+        return newStatus;
+    }
+    useEffect(()=>{
+        if(matches.length ===2) {
+            setTimeout(()=>{
+                updateGameStatus(getUpdatedStatus());
+                setMatches([]);
+            },3000)        }
+       
+    },[gameStatus])
     return <div>
     <h1>Memory Game</h1>
     <div className="parent">
-        {GRID.map((item) => {
-            return  <Card value={item}></Card>
+        {gameStatus.map((item, index) => {
+            return  item && <Card item={item} onCardClickHandler={onCardClickHandler} key={index}></Card>
         })}
        
     </div>
